85f48651363169a6c5fe4bee93408fa6714bdf3f
Mauricio Ulargui <ulargm@amazon.com>
2022-12-25 11:30:56 -0800

    infra update

--- infra/aws/scripts/deploy.sh
+++ infra/aws/scripts/deploy.sh
@@ -1,3 +1,7 @@
+#install packages
+pip install -q --upgrade pip
+pip install -q --upgrade boto3
+
 # Absolute path to this file
 SCRIPT=$(readlink -f "$0")
 export REPOPATH=$(dirname "$SCRIPT" | sed 's/\/infra\/aws\/scripts//g')
