4262e4fac92627e1e7594f8a741d2d644c3a0126
Mauricio Ulargui <ulargm@amazon.com>
2022-12-25 11:35:53 -0800

    infra update

--- infra/aws/src/main.py
+++ infra/aws/src/main.py
@@ -12,3 +12,21 @@ if __name__ == '__main__':
         sys.exit(0)
 
     print ('no role')
+
+    policy={
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:PutObject",
+                "s3:DeleteObject",
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::*"
+            ]
+        }
+    ]
+}
