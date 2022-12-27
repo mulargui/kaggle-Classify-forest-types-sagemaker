2c8cf1dfa19667b7f929063f1b0a253003f09aa0
Mauricio Ulargui <ulargm@amazon.com>
2022-12-26 19:02:02 -0800

    bug fixing

--- infra/aws/src/clear.py
+++ infra/aws/src/clear.py
@@ -4,7 +4,7 @@ import boto3
 #remove sagemaker role
 rolename="AmazonSageMaker-ExecutionRole"
 awsmanagedpolicy = 'arn:aws:iam::aws:policy/AmazonSageMakerFullAccess'
-'''    
+
 client=boto3.client('iam')
 client.detach_role_policy(
     RoleName=rolename,
@@ -22,16 +22,14 @@ bucketname = [b['Name'] for b in bucketlist['Buckets'] if "sagemaker" in b['Name
 if bucketname:
     boto3.resource('s3').Bucket(bucketname[0]).objects.delete()
     boto3.resource('s3').Bucket(bucketname[0]).delete()
-'''
+
 #remove sagemaker endpoint
 endpointname='predict-forest-type'
 
 client = boto3.client('sagemaker')
 response = client.describe_endpoint_config(EndpointConfigName=endpointname)
-print(response)
-print(response['ProductionVariants'][0]['ModelName'])
-#client.delete_endpoint(EndpointName=endpointname)
-#client.delete_endpoint_config(EndpointConfigName=endpointname)
-#client.delete_model(ModelName=response['ProductionVariants'][0]['ModelName'])
+client.delete_endpoint(EndpointName=endpointname)
+client.delete_endpoint_config(EndpointConfigName=endpointname)
+client.delete_model(ModelName=response['ProductionVariants'][0]['ModelName'])
                         
 print('teared down all aws sagemaker related resources!')
