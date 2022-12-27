6db817f913af794366299d6e37d3ff5358bfee49
Mauricio Ulargui <ulargm@amazon.com>
2022-12-26 16:59:31 -0800

    bug fixing

--- infra/aws/src/clear.py
+++ infra/aws/src/clear.py
@@ -3,9 +3,13 @@ import boto3
 
 #remove sagemaker role
 rolename="AmazonSageMaker-ExecutionRole"
+awsmanagedpolicy = 'arn:aws:iam::aws:policy/AmazonSageMakerFullAccess'
     
 client=boto3.client('iam')
-client.delete_role(RoleName=rolename)
+client.detach_role_policy(
+    RoleName=rolename,
+    PolicyArn=awsmanagedpolicy
+)client.delete_role(RoleName=rolename)
 
 #remnove S3 bucket
 bucket_name = "sagemaker-us-east-1-867679111813"
