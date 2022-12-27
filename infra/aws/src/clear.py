32591701cf40fa37c0691ea652d3e678500cab19
Mauricio Ulargui <ulargm@amazon.com>
2022-12-26 17:20:38 -0800

    bug fixing

--- infra/aws/src/clear.py
+++ infra/aws/src/clear.py
@@ -18,29 +18,19 @@ bucket_name = "sagemaker-us-east-1-867679111813"
 # Retrieve the list of existing buckets
 client = boto3.client('s3')
 bucketlist = client.list_buckets()
-bucket = [b for b in bucketlist['Buckets'] if "sagemaker" in b['Name']]
+bucket = [b['Name'] for b in bucketlist['Buckets'] if "sagemaker" in b['Name']]
 
+#delete all objects in the bucket
 if bucket:
-    print(bucket) 
-else:
-    print('no bucket')
-sys.exit(0)
-
-
-
-
-# First we list all files in bucket
-client=boto3.client('s3')
-response = client.list_objects_v2(Bucket=bucket_name, Prefix="/")
-files_in_folder = response["Contents"]
-files_to_delete = []
-# We will create Key array to pass to delete_objects function
-for f in files_in_folder:
-    files_to_delete.append({"Key": f["Key"]})
-# This will delete all files in the bucket
-response = client.delete_objects(
-    Bucket=bucket_name, Delete={"Objects": files_to_delete}
-)
+    response = client.list_objects_v2(Bucket=bucket, Prefix="/")
+    files_in_folder = response["Contents"]
+    files_to_delete = []
+    # We will create Key array to pass to delete_objects function
+    for f in files_in_folder:
+        files_to_delete.append({"Key": f["Key"]})
+    client.delete_objects(
+        Bucket=bucket, Delete={"Objects": files_to_delete}
+    )
 
 #remove sagemaker endpoint
  
