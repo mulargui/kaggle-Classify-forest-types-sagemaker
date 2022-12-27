d79ddf3421ae0e902dd99e99bd14bc99b82daa32
Mauricio Ulargui <ulargm@amazon.com>
2022-12-26 17:35:53 -0800

    bug fixing

--- infra/aws/src/clear.py
+++ infra/aws/src/clear.py
@@ -16,20 +16,17 @@ client.delete_role(RoleName=rolename)
 bucket_name = "sagemaker-us-east-1-867679111813"
 
 #find the sagemaker bucket
-client = boto3.resource('s3')
+client = boto3.client('s3')
 bucketlist = client.list_buckets()
 bucket = [b['Name'] for b in bucketlist['Buckets'] if "sagemaker" in b['Name']]
 print (bucket)
-'''
+
 #delete all objects in the bucket and the bucket
 if bucket:
     client = boto3.resource('s3')   
     bucket = client.Bucket(bucket)
     bucket.objects.all().delete()
-
-'''
-
-
+    bucket.delete()
 
 #remove sagemaker endpoint
  
