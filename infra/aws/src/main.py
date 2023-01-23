4d565120c3c6541df3e88e8bf6901fbe7b7ca7ed
Mauricio Ulargui <ulargm@amazon.com>
2023-01-23 15:43:11 -0800

    creating modules

--- infra/aws/src/main.py
+++ infra/aws/src/main.py
@@ -1,11 +1,11 @@
 import sys
 import boto3
-from .....modules.iam import Role
+from ....modules import iam
 
 if __name__ == '__main__':
 
     for path in sys.path:
         print(path)
 
-    #role = Role()
+    role = Role()
     print('Role created!')
