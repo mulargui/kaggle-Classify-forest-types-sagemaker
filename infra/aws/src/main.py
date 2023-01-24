142abb74415418a0fc5371c6402bf0c19b8b5aab
Mauricio Ulargui <ulargm@amazon.com>
2023-01-23 17:09:07 -0800

    update modules

--- infra/aws/src/main.py
+++ infra/aws/src/main.py
@@ -3,7 +3,8 @@ import boto3
 
 if __name__ == '__main__':
 
-    sys.path.insert(1, '/repo')
+    #sys.path.insert(1, '/repo')
+    sys.path.append('/repo')
     from modules.iam import Role
 
     role = Role()
