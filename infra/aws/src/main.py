b787df5229de50e2a3d857e256b764b28a1966f6
Mauricio Ulargui <ulargm@amazon.com>
2023-01-23 16:35:44 -0800

    include modules

--- infra/aws/src/main.py
+++ infra/aws/src/main.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 import boto3
 
 if __name__ == '__main__':
@@ -6,7 +6,11 @@ if __name__ == '__main__':
     for path in sys.path:
         print(path)
 
-    from ....modules import iam
+    sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+    for path in sys.path:
+        print(path)
 
-    role = iam.Role()
+    #from ....modules import iam
+
+    #role = iam.Role()
     print('Role created!')
