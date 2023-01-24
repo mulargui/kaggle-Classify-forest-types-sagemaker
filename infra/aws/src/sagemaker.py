059d6e4be39de060f412cf44e5b37ae667511eba
Mauricio Ulargui <ulargm@amazon.com>
2023-01-24 14:08:45 -0800

    aws modules

--- infra/aws/src/sagemaker.py
+++ infra/aws/src/sagemaker.py
@@ -8,11 +8,16 @@ class AWSSageMaker:
     #remnove sagemaker resources
     def remove(self):
         client = boto3.client('sagemaker')
-        response = client.describe_endpoint_config(EndpointConfigName=self.endpointname)
-        client.delete_endpoint(EndpointName=self.endpointname)
-        client.delete_endpoint_config(EndpointConfigName=self.endpointname)
 
-        client.delete_model(ModelName=response['ProductionVariants'][0]['ModelName'])
+        try:
+            response = client.describe_endpoint_config(EndpointConfigName=self.endpointname)
+
+            client.delete_endpoint(EndpointName=self.endpointname)
+            client.delete_endpoint_config(EndpointConfigName=self.endpointname)
+
+            client.delete_model(ModelName=response['ProductionVariants'][0]['ModelName'])
+        except ClientError as error:
+            print('No sagemaker resources...')
 
         return 1
  
